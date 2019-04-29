7925bd07307b635cba04d3d8ed610665328be60e
vogak <pereginp@gmail.com>
2019-04-29 16:04:13 +0300

    For check my master

--- tests/index.php
+++ tests/index.php
@@ -10,12 +10,10 @@ $form_sel = 'json'; // json , yaml , xml , php
 
 # Assignment
 $array = new Person($form_sel);
-//$json = new Person(new json);
 $array->setName('john');
 $array->setEmail('bornJ@mail.com');
 
 # Conclusion
 print_r($array);
-//$json =new Person(new json);
 print_r( $array->getSerialize($array) . PHP_EOL);
 
